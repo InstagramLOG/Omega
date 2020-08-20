8cfde33010296360b49420d2b6d79e3b9e2e3678
InstagramLOG <69980827+InstagramLOG@users.noreply.github.com>
2020-08-20 21:51:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBGe_KBrjoI-108fSVUy7qwCP7U-zPfeo4",
+  authDomain: "instagram-a9aba.firebaseapp.com",
+  databaseURL: "https://instagram-a9aba.firebaseio.com",
+  projectId: "instagram-a9aba",
+  storageBucket: "instagram-a9aba.appspot.com",
+  messagingSenderId: "609853575645",
+  appId: "1:609853575645:web:f35416c335812a46916634",
+  measurementId: "G-8TDXZ7TYB8"
 };
-
      
  
 ////Do Not Edit Below This
